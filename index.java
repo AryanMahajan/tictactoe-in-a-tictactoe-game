0c368721b4dec14b68481c7cce2ff30ede301645
AryanMahajan <96971558+AryanMahajan@users.noreply.github.com>
2024-04-11 15:55:00 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,20 +1,89 @@
 import java.util.*;
 
 public class index {
+    static int moves = 0;
+    static boolean game = true;
+
     static class BigGridelement {
-        char[][] grid;
+        char[][] grid = new char[3][3];
         char whoses = ' ';
         boolean checker = false;
+        
     }
 
-    public static void game(BigGridelement[][] grid) {
+    public static void game(BigGridelement[][] board, int board_choice, char player) {
+        Scanner sc= new Scanner(System.in);
+        print(null);
+        System.out.print("Enter the coordinates in "+board_choice+" (like x y not like x,y): ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
 
-    }
-
-    public static void print(char[][] arr) {
-        for (int i = 0; i < 9; i++) {
-            
+        switch (board_choice) {
+            case 1:
+                playin(board[0][0], player, x, y, board_choice);
+                break;
+            case 2:
+                playin(board[0][1], player, x, y, board_choice);
+                break;
+            case 3:
+                playin(board[0][2], player, x, y, board_choice);
+                break;
+            case 4:
+                playin(board[1][0], player, x, y, board_choice);
+                break;
+            case 5:
+                playin(board[1][1], player, x, y, board_choice);
+                break;
+            case 6:
+                playin(board[1][2], player, x, y, board_choice);
+                break;
+            case 7:
+                playin(board[2][0], player, x, y, board_choice);
+                break;
+            case 8:
+                playin(board[2][1], player, x, y, board_choice);
+                break;
+            case 9:
+                playin(board[2][2], player, x, y, board_choice);
+                break;
+        
+            default:
+                break;
         }
+    }
+
+    public static void playin(BigGridelement cell,char player, int x, int y, int board_choice) {
+        if(cell.grid[x][y]==' '){
+            cell.grid[x][y] =player;
+            moves++;
+            if(moves > 18){
+                gameover = haveWon(board, player);
+            }
+            if(gameover){
+                System.out.println(player+"Have won the game");
+            }else{
+                player = player == 'X' ? '0' : 'X';
+                if(row == 0 && col < 3){
+                    mboard = col;
+                }
+                if (row == 1 && col < 3 ){
+                    mboard = col+3;
+                }
+                if (row == 2 && col < 3){
+                    mboard = col+6;
+                }
+            }
+        }
+        else{
+            System.out.println("Invalid move");
+        }
+        
+        board_choice = y+1;
+        player = 'O';
+    }
+
+    public static void print(BigGridelement[][] board){
+        
     }
 
     public static boolean smallerGrid(char[][] matrix) {
@@ -22,43 +91,39 @@ public class index {
         return false;
     }
 
-    public static void whereto(int x, int y) {
-
-    }
-
     public static char finalcheck(BigGridelement bg0, BigGridelement bg1, BigGridelement bg2, BigGridelement bg3,
             BigGridelement bg4, BigGridelement bg5, BigGridelement bg6, BigGridelement bg7, BigGridelement bg8,
-            char whoses) {
+            char player) {
 
         // --- test
         if (((bg0.checker && bg1.checker && bg2.checker) || (bg3.checker && bg4.checker && bg5.checker)
-                || (bg6.checker && bg7.checker && bg8.checker)) && whoses == 'X') {
+                || (bg6.checker && bg7.checker && bg8.checker)) && player == 'X') {
             return 'X';
         } else if (((bg0.checker && bg1.checker && bg2.checker) || (bg3.checker && bg4.checker && bg5.checker)
-                || (bg6.checker && bg7.checker && bg8.checker)) && whoses == 'O') {
+                || (bg6.checker && bg7.checker && bg8.checker)) && player == 'O') {
             return 'O';
         }
 
         // ||| test
         else if (((bg0.checker && bg3.checker && bg6.checker) || (bg1.checker && bg4.checker && bg7.checker)
-                || (bg2.checker && bg5.checker && bg8.checker)) && whoses == 'X') {
+                || (bg2.checker && bg5.checker && bg8.checker)) && player == 'X') {
             return 'X';
         } else if (((bg0.checker && bg3.checker && bg6.checker) || (bg1.checker && bg4.checker && bg7.checker)
-                || (bg2.checker && bg5.checker && bg8.checker)) && whoses == 'O') {
+                || (bg2.checker && bg5.checker && bg8.checker)) && player == 'O') {
             return 'O';
         }
 
         // /// test
-        else if ((bg3.checker && bg5.checker && bg7.checker) && whoses == 'X') {
+        else if ((bg3.checker && bg5.checker && bg7.checker) && player == 'X') {
             return 'X';
-        } else if ((bg3.checker && bg5.checker && bg7.checker) && whoses == 'O') {
+        } else if ((bg3.checker && bg5.checker && bg7.checker) && player == 'O') {
             return 'O';
         }
 
         // \\\ test
-        else if ((bg0.checker && bg5.checker && bg8.checker) && whoses == 'X') {
+        else if ((bg0.checker && bg5.checker && bg8.checker) && player == 'X') {
             return 'X';
-        } else if ((bg0.checker && bg5.checker && bg8.checker) && whoses == 'O') {
+        } else if ((bg0.checker && bg5.checker && bg8.checker) && player == 'O') {
             return 'O';
         }
 
@@ -66,11 +131,11 @@ public class index {
 
     }
 
-    public static void changewhoses(BigGridelement a, char turn) {
+    public static void changewhoses(BigGridelement a, char player) {
         if (a.whoses == ' ') {
-            a.whoses = turn;
+            a.whoses = player;
         } else {
-            System.out.println("This place is already taken!");
+            System.out.println("");
         }
     }
 
@@ -83,30 +148,37 @@ public class index {
         BigGridelement bg5 = new BigGridelement();
         BigGridelement bg6 = new BigGridelement();
         BigGridelement bg7 = new BigGridelement();
-        BigGridelement bg8 = new BigGridelement();
+        BigGridelement bg8 = new BigGridelement(); 
 
-        bg0.grid = new char[3][3];
-        bg1.grid = new char[3][3];
-        bg2.grid = new char[3][3];
-        bg3.grid = new char[3][3];
-        bg4.grid = new char[3][3];
-        bg5.grid = new char[3][3];
-        bg6.grid = new char[3][3];
-        bg7.grid = new char[3][3];
-        bg8.grid = new char[3][3];
+        BigGridelement[][] board = new BigGridelement[3][3];
+        board[0][0] = bg0;
+        board[0][1] = bg1;
+        board[0][2] = bg2;
+        board[1][0] = bg3;
+        board[1][1] = bg4;
+        board[1][2] = bg5;
+        board[2][0] = bg6;
+        board[2][1] = bg7;
+        board[2][2] = bg8;
 
-        BigGridelement[][] grid = new BigGridelement[3][3];
-        grid[0][0] = bg0;
-        grid[0][1] = bg1;
-        grid[0][2] = bg2;
-        grid[1][0] = bg3;
-        grid[1][1] = bg4;
-        grid[1][2] = bg5;
-        grid[2][0] = bg6;
-        grid[2][1] = bg7;
-        grid[2][2] = bg8;
-
-        game(grid);
+        for(int boards = 0; boards < 3; boards++){
+            for(int boards2 = 0; boards2 < 3; boards2++){
+                for(int i = 0; i < 3; i++){
+                    for (int j = 0; j < 3; j++) {
+                        board[boards][boards2].grid[i][j]= ' ';
+                    }
+                }
+            }
+        }
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Which board do you want to start with: ");
+        int board_choice = sc.nextInt();        
+        sc.nextLine();
+        char player = 'X';
+        while(game){
+            game(board,board_choice,player);
+        }
 
     }
 }
